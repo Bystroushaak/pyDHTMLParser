83e2f316fbb85b4e8d927ded97cbfd2b31d6831f
Bystroushaak <bystrousak@kitakitsune.org>
2014-07-29 17:59:37 +0200

    #10: Added docstrings to all functions without proper docstrings. #4: Refactored everything that could be.

--- src/dhtmlparser/__init__.py
+++ src/dhtmlparser/__init__.py
@@ -154,10 +154,14 @@ def _repair_tags(taglist):
 
 def _indexOfEndTag(istack):
     """
-    Go through istack and search endtag. Element at first index is considered
+    Go through `istack` and search endtag. Element at first index is considered
     as opening tag.
 
-    Returns: index of end tag or 0 if not found.
+    Args:
+        istack (list): List of :class:`.HTMLElement` objects.
+
+    Returns:
+        int: Index of end tag or 0 if not found.
     """
     if len(istack) <= 0:
         return 0
@@ -165,27 +169,32 @@ def _indexOfEndTag(istack):
     if not istack[0].isOpeningTag():
         return 0
 
-    opener = istack[0]
     cnt = 0
-
-    index = 0
-    for el in istack[1:]:
+    opener = istack[0]
+    for index, el in enumerate(istack[1:]):
         if el.isOpeningTag() and \
            el.getTagName().lower() == opener.getTagName().lower():
             cnt += 1
+
         elif el.isEndTagTo(opener):
             if cnt == 0:
                 return index + 1
-            else:
-                cnt -= 1
 
-        index += 1
+            cnt -= 1
 
     return 0
 
 
 def _parseDOM(istack):
-    "Recursively go through element array and create DOM."
+    """
+    Recursively go through element array and create DOM.
+
+    Args:
+        istack (list): List of :class:`.HTMLElement` objects.
+
+    Returns:
+        list: DOM tree as list.
+    """
     ostack = []
     end_tag_index = 0
 
@@ -193,21 +202,24 @@ def _parseDOM(istack):
     while index < len(istack):
         el = istack[index]
 
-        end_tag_index = _indexOfEndTag(istack[index:])  # Check if this is pair tag
+        # check if this is pair tag
+        end_tag_index = _indexOfEndTag(istack[index:])
 
         if not el.isNonPairTag() and end_tag_index == 0 and not el.isEndTag():
             el.isNonPairTag(True)
 
-        if end_tag_index != 0:
-            el.childs = _parseDOM(istack[index + 1: end_tag_index + index])
-            el.endtag = istack[end_tag_index + index]  # Reference to endtag
-            el.endtag.openertag = el
-            ostack.append(el)
-            ostack.append(el.endtag)
-            index = end_tag_index + index
-        else:
+        if end_tag_index == 0:
             if not el.isEndTag():
                 ostack.append(el)
+        else:
+            el.childs = _parseDOM(istack[index + 1: end_tag_index + index])
+            el.endtag = istack[end_tag_index + index]  # reference to endtag
+            el.endtag.openertag = el
+
+            ostack.append(el)
+            ostack.append(el.endtag)
+
+            index = end_tag_index + index
 
         index += 1
 
@@ -216,19 +228,27 @@ def _parseDOM(istack):
 
 def parseString(txt, cip=True):
     """
-    Parse given string and return DOM tree consisting of single linked
-    HTMLElements.
+    Parse string `txt` and return DOM tree consisting of single linked
+    :class:`.HTMLElement`.
+
+    Args:
+        txt (str): HTML/XML string, which will be parsed to DOM.
+        cip (bool, default True): Case Insensitive Parameters. Use special
+            dictionary to store :attr:`.HTMLElement.params` as case insensitive.
+
+    Returns:
+        obj: Single conteiner HTML element with blank tag, which has whole DOM \
+             in it's :attr:`.HTMLElement.childs` property. This element can be \
+             queried using :meth:`.HTMLElement.find` functions.
     """
     # remove UTF BOM (prettify fails if not)
     if len(txt) > 3 and txt.startswith("\xef\xbb\xbf"):
         txt = txt[3:]
 
     if not cip:
-        htmlelement.SpecialDict = []
-        reload(htmlelement)
-    elif htmlelement.SpecialDict == []:
+        htmlelement.SpecialDict = dict
+    elif type(htmlelement.SpecialDict) == dict:
         htmlelement.SpecialDict = specialdict.SpecialDict
-        reload(htmlelement)
 
     container = HTMLElement()
     container.childs = _parseDOM(
@@ -242,8 +262,13 @@ def parseString(txt, cip=True):
 
 def makeDoubleLinked(dom, parent=None):
     """
-    Standard output from dhtmlparser is single-linked tree. This will make it
+    Standard output from `dhtmlparser` is single-linked tree. This will make it
     double-linked.
+
+    Args:
+        dom (obj): :class:`.HTMLElement` instance.
+        parent (obj, default None): Don't use this, it is used in recursive
+               call.
     """
     dom.parent = parent
 
@@ -254,34 +279,34 @@ def makeDoubleLinked(dom, parent=None):
 
 def removeTags(dom):
     """
-    Remove all tags from dom, so result should be plaintext.
+    Remove all tags from `dom` and obtain plaintext representation.
 
-    dom -- string, HTMLElement or just array of elements.
+    Args:
+        dom (str, obj, array): str, HTMLElement instance or array of elements.
+
+    Returns:
+        str: Plain string without tags.
     """
-    output = ""
-
     # initialize stack with proper value (based on dom parameter)
     element_stack = None
-    if isinstance(dom, list) or isinstance(dom, tuple):
+    if type(dom) in [list, tuple]:
         element_stack = dom
     elif isinstance(dom, HTMLElement):
-        if not dom.isTag():
-            element_stack = [dom]
-        else:
-            element_stack = dom.childs
+        element_stack = dom.childs if dom.isTag() else [dom]
     elif isinstance(dom, str):
         element_stack = parseString(dom).childs
     else:
         element_stack = dom
 
     # remove all tags
-    while len(element_stack) > 0:
+    output = ""
+    while element_stack:
         el = element_stack.pop(0)
 
-        if not (el.isTag() or el.isComment() or el.getTagName() == ""):
+        if not (el.isTag() or el.isComment() or not el.getTagName()):
             output += str(el)
 
-        if len(el.childs) > 0:
+        if el.childs:
             element_stack = el.childs + element_stack
 
     return output
