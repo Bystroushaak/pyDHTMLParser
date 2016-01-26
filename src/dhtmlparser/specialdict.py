dd8d6180a25c57aa3b249464e45d4eaa50ede32f
Bystroushaak <bystrousak@kitakitsune.org>
2016-01-26 16:46:15 +0100

    Mostly working implementation. But there is one catch:
    https://www.reddit.com/r/learnpython/comments/42rqee/help_dict_subclassing_and_dictmy_subclass/

--- src/dhtmlparser/specialdict.py
+++ src/dhtmlparser/specialdict.py
@@ -30,37 +30,96 @@ class SpecialDict(OrderedDict):
     This dictionary stores items case sensitive, but compare them case
     INsensitive.
     """
-    def __contains__(self, key):
-        keys = super(SpecialDict, self).keys()
-        return _lower_if_str(key) in {_lower_if_str(key) for key in keys}
+    def __init__(self, *args, **kwargs):
+        self._case = OrderedDict()
+        super(SpecialDict, self).__init__(*args, **kwargs)
+
+    def __setitem__(self, key, value):
+        lower_key = _lower_if_str(key)
+        self._case[lower_key] = key
+
+        super(SpecialDict, self).__setitem__(lower_key, value)
 
     def __getitem__(self, key):
-        key = _lower_if_str(key)
+        return super(SpecialDict, self).__getitem__(_lower_if_str(key))
 
-        for item in self.keys():
-            if key == _lower_if_str(item):
-                return super(SpecialDict, self).__getitem__(item)
+    def __delitem__(self, key):
+        lower_key = _lower_if_str(key)
 
-        raise KeyError("Can't find key '%s'!" % key)
+        # in case of popitem() this is not true, because it is poped before
+        if lower_key in self._case:
+            del self._case[lower_key]
 
-    def __eq__(self, obj):
-        if self is obj:
-            return True
+        return super(SpecialDict, self).__delitem__(lower_key)
 
-        if not hasattr(obj, "keys"):
-            return False
+    def clear(self):
+        self._case.clear()
+        return super(SpecialDict, self).clear()
 
-        if len(self.keys()) != len(obj.keys()):
-            return False
+    def keys(self):
+        return self._case.values()
 
-        for key in obj.keys():
-            if not self.__contains__(key):
-                return False
+    def iterkeys(self):
+        return self._case.itervalues()
 
-            if obj[key] != self.__getitem__(key):
-                return False
+    def items(self):
+        return zip(self.iterkeys(), self.itervalues())
 
-        return True
+    def iteritems(self):
+        value_iterator = iter(self.itervalues())
 
-    def __ne__(self, obj):
-        return not self.__eq__(obj)
+        for key in self._case.itervalues():
+            yield key, next(value_iterator)
+
+    def get(self, k, d=None):
+        return super(SpecialDict, self).get(_lower_if_str(k), d)
+
+    def popitem(self, ):
+        lower_key, key = self._case.popitem()
+        return key, self.pop(lower_key)
+
+    def __iter__(self):
+        return self._case.itervalues()
+
+    def __contains__(self, key):
+        return super(SpecialDict, self).__contains__(_lower_if_str(key))
+
+    def has_key(self, key):
+        return key in self
+
+    # def __eq__(self, obj):
+    #     if self is obj:
+    #         return True
+
+    #     if not hasattr(obj, "keys"):
+    #         return False
+
+    #     if len(self.keys()) != len(obj.keys()):
+    #         return False
+
+    #     for key in obj.keys():
+    #         if not self.__contains__(key):
+    #             return False
+
+    #         if obj[key] != self.__getitem__(key):
+    #             return False
+
+    #     return True
+
+    # def __ne__(self, obj):
+    #     return not self.__eq__(obj)
+
+    # def __getattribute__(self, name):
+    #     attr = object.__getattribute__(self, name)
+    #     print name
+    #     if hasattr(attr, '__call__'):
+    #         print "%s has __call__" % name
+    #         def newfunc(*args, **kwargs):
+    #             print('before calling %s' %attr.__name__)
+    #             result = attr(*args, **kwargs)
+    #             print('done calling %s' %attr.__name__)
+    #             return result
+    #         return newfunc
+    #     else:
+    #         print "attr", name, attr
+    #         return attr
